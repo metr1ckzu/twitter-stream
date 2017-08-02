9e24755e8b44574b2a3ec5f8951cfd23f0363ae1
metr1ckzu <metr1ckzu@gmail.com>
2017-08-03 05:41:11 +0600

    added user feed, template

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
+from stream_twitter import views
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^tweet/', login_required(views.TweetView.as_view()))
+)
