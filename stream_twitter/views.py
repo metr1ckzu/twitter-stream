68c805c9cee7d6191704f70225f1ab6e6ce12cb9
metr1ckzu <metr1ckzu@gmail.com>
2017-08-03 05:49:17 +0600

    added feed follow

--- stream_twitter/views.py
+++ stream_twitter/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from stream_twitter.models import Follow
 from stream_twitter.models import Tweet
+from django.contrib.auth.models import user
+from stream_django.enrich import Enrich
+from stream_django.feed_manager import feed_manager
 
 # Create your views here.
 
@@ -13,3 +16,25 @@ class TweetView(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(Tweet, self).form_valid(form)
+
+
+def profile_feed(request, username=None):
+    enricher = Enrich()
+    user = user.objects.get(username=username)
+    feed = feed_manager.get_user_feed(user.id)
+    activities = feed.get(limit=25)['results']
+    enricher.enrich_activities(activities)
+    context = {
+        'activities': activities
+    }
+    return render(request, 'tweets.html', context)
+
+
+class FollowView(CreateView):
+    model = Follow
+    fields = ['target']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(Tweet, self).form_valid(form)
+
